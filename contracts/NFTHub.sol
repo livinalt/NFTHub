ebf43b85a2992fbbf76c7221abb18e49756ef16c
Livinalt <livinalt@gmail.com>
2024-09-20 19:39:56 +0100

    feat:: Added Events to NFTHub.sol

--- contracts/NFTHub.sol
+++ contracts/NFTHub.sol
@@ -15,8 +15,11 @@ contract NFTHub{
     address public paymentToken;  // could be usdt, usdc etc...
 
     address public owner;
-    ERC721 public nftContractAddress; // this is the address that you want to mint
 
+    event NFTMinted(address indexed minter, uint256 indexed tokenId);
+    event Listed(uint256 indexed _tokenId, address indexed owner, uint256 indexed _price, bool);
+    event NFTBought(address indexed from, address indexed to, uint256 indexed _tokenId);
+    event OwnershipTransfered(address indexed owner, address indexed newOwner, uint256 indexed _tokenId);
 
  struct ListedNFT{
     uint256 tokenId;
@@ -33,9 +36,15 @@ contract NFTHub{
 
 
 
-constructor(){
+constructor( address _paymentToken){
     owner = msg.sender;
+    paymentToken = _paymentToken;
    
+}
+
+modifier onlyOwner(){
+    require(msg.sender == owner,"Not Only");
+    _;
 }
  
 // for the user to mint an already existing nft 
@@ -44,9 +53,11 @@ constructor(){
         require(tokenId != 0, "Invalid token Id");
         nftToken.safeMint(msg.sender, tokenId);
 
+        emit NFTMinted(msg.sender, tokenId);
+
  }
 
- function listNFT(uint256 _tokenId, uint256 _price) external{
+ function listNFT(uint256 _tokenId, uint256 _price) external onlyOwner{
         require(_price != 0, "Address Zero Detected");
         require(_tokenId != 0, "Invalid token Id");
 
@@ -57,6 +68,8 @@ constructor(){
         nft.isListed = true;
 
         allListedNFT.push(nft);
+
+        emit Listed(_tokenId, msg.sender, _price, true);
 
  }
 // buy involves transfereing ownership from one owner to another
@@ -78,15 +91,19 @@ constructor(){
         nft.isListed = false;
         nft.owner = msg.sender;
 
+        emit NFTBought(address(this), msg.sender, _tokenId);
+
  }
 
  
 // this transfers ownership of a token from one user to another
- function transferOwnership(uint256 _tokenId) external {
+ function transferOwnership(uint256 _tokenId) external onlyOwner{
 
     ListedNFT memory nft = ListedNftId[_tokenId];
 
     nftToken.safeTransferFrom(nft.owner, msg.sender, _tokenId);
+
+    emit OwnershipTransfered(nft.owner, msg.sender, _tokenId);
 
  }
 
